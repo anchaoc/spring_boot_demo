e764ce5062e8823a498d7ee133175a552e3efaa4
anchao <240853850@qq.com>
2019-09-02 00:53:12 +0800

    添加邮件配置

---
+++ src/test/java/com/ac/SpringBootDemoTest.java
@@ -0,0 +1,56 @@
+package com.ac;
+
+import com.ac.otherserver.MailServer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author anchao
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoTest {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private MailServer mailServer;
+
+//    @Test
+//    public void test() throws MessagingException, UnsupportedEncodingException {
+//        String id ="new_001";
+//        String text ="<html>\n"+
+//                "<body>\n"+
+//                "<h4>hello world，这是一封含图片邮件:</h4><img src=\'cid:"+id+"\'/>"+
+//                "</body>\n</html>";
+//        String to ="13182370572@163.com";
+//        String subject ="来自JAVA_MAIL";
+//        String filePath ="D:/资料/images/手捧花.jpg";
+//
+//        mailServer.sendImageMail(to,subject,text,filePath,id);
+//    }
+
+//    @Test
+//    public void test2(){
+//        Context context = new Context();
+//        context.setVariable("id","007");
+//        String emailTemplate = templateEngine.process("emailTemplate", context);
+//
+//        String to ="13182370572@163.com";
+//        String subject ="这是一个模板邮件";
+//
+//        mailServer.sendHTMLMail(to,subject,emailTemplate);
+//    }
+
+
+
+}
