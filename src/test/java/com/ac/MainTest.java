fe47d755217a524c19e7e548fd984d465ad438e0
anchao <240853850@qq.com>
2019-10-09 14:45:33 +0800

    main 测试

---
+++ src/test/java/com/ac/MainTest.java
@@ -0,0 +1,12 @@
+package com.ac;
+
+/**
+ * @author anchao
+ * @date 2019/10/9 14:42
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperties());
+    }
+}
