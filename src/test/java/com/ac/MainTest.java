689b68a28dfba837d1937ecdcfda3e8d71657af3
anchao <240853850@qq.com>
2019-10-16 20:24:21 +0800

    main 测试

--- src/test/java/com/ac/MainTest.java
+++ src/test/java/com/ac/MainTest.java
@@ -1,5 +1,7 @@
 package com.ac;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * @author anchao
  * @date 2019/10/9 14:42
@@ -7,6 +9,19 @@ package com.ac;
 public class MainTest {
 
     public static void main(String[] args) {
-        System.out.println(System.getProperties());
+        testAtomic();
     }
+
+
+    /**
+     * 并发保证原子性
+     */
+    private static void testAtomic(){
+        AtomicReference<Integer> one = new AtomicReference<>(1);
+        Integer oneGet = one.get();
+        one.compareAndSet(oneGet,2);
+        System.out.println(one.get());
+    }
+
+
 }
