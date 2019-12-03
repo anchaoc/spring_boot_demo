1574d7037c5ae644173c6eaf047ca659bf4ca5da
anchao <240853850@qq.com>
2019-12-03 15:53:50 +0800

    测试类

--- src/test/java/com/ac/MainTest.java
+++ src/test/java/com/ac/MainTest.java
@@ -4,6 +4,7 @@ import com.ac.utils.GsonUtil;
 import com.google.common.collect.Lists;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -19,7 +20,6 @@ import java.util.stream.Collectors;
 public class MainTest {
 
     public static void main(String[] args) {
-        test7();
     }
 
 
