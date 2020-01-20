330b2e70c9e1a6a1686c4e98402c1b19a93245d6
anchao <240853850@qq.com>
2020-01-20 21:00:36 +0800

    mybatis

--- demo_web/src/test/java/com/ac/MainTest.java
+++ demo_web/src/test/java/com/ac/MainTest.java
@@ -24,8 +24,28 @@ import java.util.stream.Collectors;
 public class MainTest {
 
     public static void main(String[] args) throws Exception {
-        test14();
+        test15();
     }
+
+
+
+    private static void test15(){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /**
