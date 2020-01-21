6439605381381cf788fe100030b3a9c299c02883
anchao <240853850@qq.com>
2020-01-21 15:29:46 +0800

    INIT

---
+++ demo_core/src/main/java/com/ac/mybatisconfig/CodeGenerator.java
@@ -0,0 +1,113 @@
+
+package com.ac.mybatisconfig;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Mybatis-plus代码生成器
+ **/
+@Slf4j
+public class CodeGenerator {
+//    /**
+//     * 读取控制台内容
+//     */
+//    public static String scanner(String tip) {
+//        Scanner scanner = new Scanner(System.in);
+//        StringBuilder help = new StringBuilder();
+//        help.append("请输入" + tip + "：");
+//        log.info(help.toString());
+//        if (scanner.hasNext()) {
+//            String ipt = scanner.next();
+//            if (StringUtils.isNotEmpty(ipt)) {
+//                return ipt;
+//            }
+//        }
+//        throw new MybatisPlusException("请输入正确的" + tip + "！");
+//    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+        Class.forName("freemarker.template.Configuration");
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = "D:/JAVABEAN";
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("anchao");
+        gc.setOpen(true);
+        gc.setServiceName("%sService");
+        gc.setBaseResultMap(true);
+        gc.setBaseColumnList(true);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/test_dev?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=Asia/Shanghai&zeroDateTimeBehavior=CONVERT_TO_NULL");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName("demo");
+        pc.setParent("com.ac");
+        pc.setEntity("core.domain");
+        pc.setMapper("core.mapper");
+        pc.setService("core.service");
+        pc.setServiceImpl("core.service.impl");
+
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        List<FileOutConfig> focList = new ArrayList<>();
+        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/src/main/resources/mapper/"
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+        mpg.setTemplate(new TemplateConfig().setXml(null));
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        // 数据库表映射到实体的命名策略
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        // 数据库表字段映射到实体的命名策略
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        // 自定义继承的Entity类全称，带包名
+        // 【实体】是否为lombok模型（默认 false）
+        strategy.setEntityLombokModel(true);
+        strategy.setInclude(
+                "qrtz_triggers"
+        );
+        // 驼峰转连字符
+        strategy.setControllerMappingHyphenStyle(true);
+        // 表前缀
+        strategy.setTablePrefix("");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
