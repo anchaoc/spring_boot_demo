cb4a28450957468b5cf8bcf29f354ba988b55e6d
anchao <240853850@qq.com>
2020-01-21 11:05:48 +0800

    清理

--- demo_core/src/main/java/com/ac/mybatisconfig/MybatisConfig.java
+++ demo_core/src/main/java/com/ac/mybatisconfig/MybatisConfig.java
@@ -1,7 +1,17 @@
 package com.ac.mybatisconfig;
 
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
+import org.apache.ibatis.mapping.DatabaseIdProvider;
+import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+
+import java.util.List;
 
 /**
  * @author anchao
@@ -9,6 +19,19 @@ import org.springframework.context.annotation.Configuration;
  */
 @MapperScan(basePackages = {"com.ac.dao"})
 @Configuration
-public class MybatisConfig {
+public class MybatisConfig extends MybatisPlusAutoConfiguration {
 
+    /**
+     * 忽略初始化时扫描com.ac包警告
+     */
+    public MybatisConfig(MybatisPlusProperties properties
+            , ObjectProvider<Interceptor[]> interceptorsProvider
+            , ResourceLoader resourceLoader
+            , ObjectProvider<DatabaseIdProvider> databaseIdProvider
+            , ObjectProvider<List<ConfigurationCustomizer>> configurationCustomizersProvider
+            , ApplicationContext applicationContext) {
+
+        super(properties, interceptorsProvider, resourceLoader, databaseIdProvider, configurationCustomizersProvider, applicationContext);
+
+    }
 }
--- demo_web/src/main/java/com/ac/config/security/WebSecurityConfig.java
+++ demo_web/src/main/java/com/ac/config/security/WebSecurityConfig.java
@@ -16,10 +16,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
     }
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
     }
 }
