ce87d4769d363343dc0d4276bff7039ba78245f3
anchao <240853850@qq.com>
2020-01-23 11:33:47 +0800

    security  test

--- demo_web/src/main/java/com/ac/controller/tax/TaxController.java
+++ demo_web/src/main/java/com/ac/controller/tax/TaxController.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.Api;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -27,7 +26,7 @@ public class TaxController {
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("add")
+    @GetMapping("add")
     public String add(){
         return "/tax/add";
     }
