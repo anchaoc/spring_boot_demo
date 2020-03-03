ea2d1aa383ce8b34dd245e19ab991699a38d2085
anchao <240853850@qq.com>
2020-03-03 14:11:09 +0800

    test

---
+++ demo_web/src/test/java/com/ac/server/MongoTemplatet.java
@@ -0,0 +1,21 @@
+package com.ac.server;
+
+/**
+ * @author anchao
+ * @date 2020/3/3 14:03
+ */
+
+public class MongoTemplatet {
+
+//    @Autowired
+//    private MongoTemplate mongoTemplate;
+//
+//       public void t(){
+//           Criteria criteria = new Criteria();
+//           criteria.in();
+//           Query query = new Query(criteria);
+//           List<String> stringList = mongoTemplate.find(query, String.class);
+//       }
+
+
+}
